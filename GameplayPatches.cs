abb37c1ffcd0ee80302c3f33fe8f3f4150d0f9e5
Ilan Ahmed <131523828+Lazycoder744@users.noreply.github.com>
2026-02-23 21:41:35 +0000

    Add pitch decompensation to replays
    How did I miss that...

---
+++ GameplayPatches.cs
@@ -0,0 +1,58 @@
+using HarmonyLib;
+using System.Reflection;
+
+namespace BSNightcore.HarmonyPatches
+{
+    [HarmonyPatch(typeof(GameplayCoreInstaller), "InstallBindings")]
+    public class GameplayCoreInstallerPatch
+    {
+        private static FieldInfo _audioManagerField;
+
+        static GameplayCoreInstallerPatch()
+        {
+            _audioManagerField = typeof(GameplayCoreInstaller).GetField("_audioManager", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        static void Postfix(GameplayCoreInstaller __instance)
+        {
+            if (!PluginConfig.Instance.ModEnabled) return;
+
+            var audioManager = _audioManagerField.GetValue(__instance) as AudioManagerSO;
+            if (audioManager != null)
+            {
+                audioManager.musicPitch = 1f;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SettingsApplicatorSO), "ApplyGameSettings")]
+    public class SettingsApplicatorPatch
+    {
+        private static FieldInfo _audioManagerField;
+
+        static SettingsApplicatorPatch()
+        {
+            _audioManagerField = typeof(SettingsApplicatorSO).GetField("_audioManager", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        static void Postfix(SettingsApplicatorSO __instance)
+        {
+            var audioManager = _audioManagerField.GetValue(__instance) as AudioManagerSO;
+            if (audioManager != null)
+            {
+                AudioManagerHolder.Instance = audioManager;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(AudioManagerSO), "set_musicPitch")]
+    public class AudioManagerPitchPatch
+    {
+        static bool Prefix(ref float value)
+        {
+            if (!PluginConfig.Instance.ModEnabled) return true;
+            value = 1f;
+            return true;
+        }
+    }
+}
